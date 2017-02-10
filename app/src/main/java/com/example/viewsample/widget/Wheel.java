41b9cbdf84bd6dd4521858c2bf41112e9be2ee67
yinlongquan <yinlongquan@conew.com>
2017-02-10 13:55:05 +0800

    modify some

--- app/src/main/java/com/example/viewsample/widget/Wheel.java
+++ app/src/main/java/com/example/viewsample/widget/Wheel.java
@@ -27,6 +27,11 @@ public class Wheel extends View {
     private float mInnerCircleMargin = 8;
     private float mInnerCircleRadius = 8;
 
+    private boolean mHint;
+    private float lastX;
+    private float lastY;
+    private float mDegree;
+
     public Wheel(Context context) {
         this(context, null);
     }
@@ -71,29 +76,91 @@ public class Wheel extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.d(TAG, "onDraw left:" + getLeft() + " top:" + getTop() + " right:" + (getLeft() + getWidth()) + " bottom:" + (getTop() + getHeight()) + " translationX:" + getTranslationX() + " getX:" + getX());
         mPaint.setColor(Color.BLUE);
         mBoundRect.set(0, 0, getWidth(), getHeight());
+
+        int cx = getWidth() /2;
+        int cy = getHeight() /2;
+        int circleRadius = getWidth() /2;
         canvas.drawRect(mBoundRect, mPaint);
+        canvas.rotate(mDegree, cx, cy);
+
+        //paint shadow
 
         mPaint.setColor(Color.RED);
-        canvas.drawCircle(getWidth() / 2, getHeight() / 2, getWidth() / 2, mPaint);
+        canvas.drawCircle(cx,cy, circleRadius, mPaint);
+
+
         mPaint.setColor(Color.WHITE);
-
-
         canvas.save();
         int translateX = (int) (getWidth() - (mInnerCircleRadius * 2 + mInnerCircleMargin));
         canvas.translate(translateX, 0);
-        canvas.drawCircle(mInnerCircleRadius, getHeight() / 2, mInnerCircleRadius, mPaint);
+        canvas.drawCircle(mInnerCircleRadius, cy, mInnerCircleRadius, mPaint);
         canvas.restore();
+
+
+
 
     }
 
 
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return true;
+        float x = event.getX();
+        float y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = x;
+                lastY = y;
+                mHint = isHint(x,y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                calculateRotationAndInvalidate(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return mHint;
+    }
+
+    private void calculateRotationAndInvalidate(float x, float y) {
+        float dx = x - lastX;
+        float dy = y - lastY;
+
+        boolean isClockwise = (dx > 0 && Math.abs(dx) >= Math.abs(dy)) || (dy > 0 && Math.abs(dy) >= Math.abs(dx));
+        float arcLength = (int) calculateDistance(dx, dy);
+        float movedDegree = (float) ((180 * arcLength / (Math.PI * getWidth() / 2)) % 360);
+        if (isClockwise) {
+            mDegree += movedDegree;
+        } else {
+            mDegree -= movedDegree;
+        }
+        lastX = x;
+        lastY = y;
+        invalidate();
+    }
+
+    private boolean isHint(float x, float y) {
+        float cx = getWidth() /2;
+        float cy = getHeight() /2;
+        double radius = getWidth() /2;
+        return distanceBetweenToPoint(x,y, cx ,cy) <= radius;
+    }
+
+    private double distanceBetweenToPoint(float x, float y, float cx, float cy) {
+        return calculateDistance(x - cx, y - cy);
+    }
+
+    /**
+     *
+     * @param dx x distance
+     * @param dy y distance
+     * @return
+     */
+    private double calculateDistance(float dx, float dy){
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
     }
 
 }
